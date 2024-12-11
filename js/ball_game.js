5a441b83d348c60dd7b519ff1438ca05c3ca5a78
Jan Jongerden <jan.jongerden@gmail.com>
2024-12-12 00:01:11 +0100

    Basic content, images, css of the website

---
+++ js/ball_game.js
@@ -0,0 +1,81 @@
+const barWidth = 200;
+let ballX = 200;
+let ballY = 200;
+let ballGoingLeft = true;
+let ballGoingUp = true;
+const ballSize = 26;
+const barOffset = 80;
+
+function initializeGame() {
+    clearSquare(maxRow - 2, 2);
+    const ball = document.createElement("img");
+    ball.id = "ball";
+    ball.src = "img/ball.webp";
+    ball.alt = "The ball";
+    document.body.appendChild(ball);
+
+    const bar = document.createElement("canvas");
+    bar.id = "bar";
+    bar.width = "500";
+    bar.height = "200";
+    let barX = 20;
+    let barY = window.innerHeight - barOffset;
+    bar.style.top = `${barY}px`;
+    document.body.appendChild(bar);
+
+    const ctx = bar.getContext("2d");
+    ctx.beginPath();
+    ctx.roundRect(0, 0, barWidth, 25, 10);
+    ctx.fillStyle = "rgba(255, 255, 0, .5)";
+    ctx.stroke();
+    ctx.fill();
+}
+
+async function playBall() {
+    initializeGame()
+    while (true) {
+        ball.style.left = `${ballX}px`;
+        ball.style.top = `${ballY}px`;
+        if (ballGoingLeft) {
+            if (ballX == 0) {
+                ballGoingLeft = false;
+            } else {
+                ballX -= 1;
+            }
+        } else {
+            if (ballX == window.innerWidth - ballSize) {
+                ballGoingLeft = true;
+            } else {
+                ballX += 1;
+            }
+        }
+        if (ballGoingUp) {
+            if (ballY == 0) {
+                ballGoingUp = false;
+            } else {
+                if (ballY % squareSize == 0) {
+                    const col = Math.floor((ballX + ballSize / 2) / squareSize);
+                    const row = Math.floor(ballY / squareSize);
+                    const square = document.getElementById(`square-${row}-${col}`);
+                    console.log();
+                }
+                ballY -= 1;
+            }
+        } else {
+            if (ballY == window.innerHeight - ballSize - barOffset) {
+                if (ballX > barX && ballX < barX + barWidth) {
+                    ballGoingUp = true;
+                } else {
+                    ballY += 1;
+                }
+            } else if (ballY == window.innerHeight) {
+                    word(20, 5, "game over")
+            } else {
+                ballY += 1;
+            }
+        }
+        await sleep(1);
+        barX = mouseX - barWidth/2
+        bar.style.left = `${barX}px`;
+    }
+}
---
+++ js/tools.js
@@ -0,0 +1,107 @@
+let mouseX = -1;
+let mouseY = -1;
+
+document.addEventListener('mousemove', (event) => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+});
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function setCharacter(row, col, char, link = "") {
+    const square = document.getElementById(`square-${row}-${col}`);
+    if (square) {
+        square.textContent = char;
+        if (link) {
+            square.addEventListener('click', function() {
+                    window.location.href = link;
+            });
+            square.classList.add("link");
+        }
+    }
+}
+
+function word(row, col, word, link = "") {
+    for (let i = 0; i < word.length; i++) {
+        setCharacter(row, col + i, word.charAt(i), link);
+    }
+}
+
+async function wrappingText(lines) {
+    const startCol = 0;
+    let row = 2;
+    let readingUrl = false;
+    let url = "";
+    for (const line of lines) {
+        let col = startCol
+        for (const char of line) {
+            if (readingUrl) {
+                if (char == '|') {
+                    readingUrl = false;
+                } else {
+                    url += char;
+                }
+            } else {
+                if (char == '{') {
+                    readingUrl = true;
+                } else if (char == '}') {
+                    url = "";
+                } else {
+                    col++;
+                    if (col == maxCol - 1) {
+                        col = 1;
+                        row++;
+                    }
+                    await sleep(30);
+                    setCharacter(row, col, char, url);
+                }
+            }
+        }
+        row++;
+    }
+}
+
+function image(row, col, imageName, title, link = "", functionName = "") {
+    const square = document.getElementById(`square-${row}-${col}`);
+    if (square) {
+        const image = document.createElement("img");
+        image.src = imageName;
+        image.title = title;
+        square.appendChild(image);
+        if (link) {
+            square.addEventListener('click', function() {
+                    window.location.href = link;
+            });
+            square.classList.add("link");
+        } else if (functionName) {
+            square.addEventListener('click', functionName);
+            square.classList.add("link");
+        }
+    }
+}
+
+function clearSquare(row, col) {
+    const square = document.getElementById(`square-${row}-${col}`);
+    if (square) {
+        square.replaceChildren();
+    }
+}
+
+function drawPage(pageName, language) {
+    let offset = 1;
+    for (const link of headerLinks) {
+        word(0, offset, link, `?page=${link}&lang=${activeLang}`);
+        offset += link.length + 2;
+    }
+    if (activeLang == "en") {
+        image(0, offset, "img/nl.png", "Nederlandse versie", `?page=${activePage}&lang=nl`);
+    } else {
+        image(0, offset, "img/uk.png", "English version", `?page=${activePage}&lang=en`);
+    }
+    const page = pages[pageName];
+    texts = language == "nl" ? page.texts_nl : page.texts_en;
+    wrappingText(texts);
+    image(maxRow - 2, 2, "img/ball.webp", "Play ball", '', playBall);
+}
